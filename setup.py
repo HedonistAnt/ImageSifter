c94e5e8c85f970eb0a15289ae7399ab598de81d4
HedonistAnt <redkandibober666@gmail.com>
2017-08-24 02:32:29 +0300

    v 1.0.0

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='imagesifter',
+    version='1.0.0',
+    packages=find_packages(),
+    author='HedonistAnt',
+    author_email='redkandibober666@gmail.com',
+    url='https://github.com/HedonistAnt/ImageSifter',
+)
