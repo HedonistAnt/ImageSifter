ba5bcd5df7f4b76288a1d45ea4eae3ffaa9bcd23
HedonistAnt <redkandibober666@gmail.com>
2017-08-24 22:49:56 +0300

    Added documentation

--- setup.py
+++ setup.py
@@ -1,10 +1,32 @@
 from setuptools import setup, find_packages
+from codecs import open
+from os import path
 
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
 setup(
     name='imagesifter',
-    version='1.0.0',
+    version='1.0.3',
+    description='Easy-to-use package for downloading originals of images from web sites ',
+
     packages=find_packages(),
-    author='HedonistAnt',
+    author='Maria_Mamaeva',
     author_email='redkandibober666@gmail.com',
     url='https://github.com/HedonistAnt/ImageSifter',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    keywords='download image from web sites',
+
+    python_requires='>=2.7'
 )
